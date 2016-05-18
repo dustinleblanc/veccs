e3e4976a86af8bfa176bccf9485131e01644f30b
Dustin LeBlanc <dustin.m.leblanc@gmail.com>
2016-05-18 09:50:09 -0400

    Some basic setup

--- RoboFile.php
+++ RoboFile.php
@@ -6,5 +6,28 @@
  */
 class RoboFile extends \Robo\Tasks
 {
-    // define public methods as commands
+  use \Boedah\Robo\Task\Drush\loadTasks;
+
+  const DRUSH_BIN = __DIR__ . '/vendor/bin/drush';
+  const DRUPAL_ROOT = __DIR__ . '/web';
+
+  // define public methods as commands
+  public function install()
+  {
+    $this->buildDrushTask()
+      ->siteInstall();
+  }
+
+  public function serve()
+  {
+    $this->taskServer(8000)
+      ->dir(self::DRUPAL_ROOT)
+      ->env(['PRESSFLOW_SETTINGS' => file_get_contents('env.json')])
+      ->run();
+  }
+
+  protected function buildDrushTask() {
+    return $this->taskDrushStack(self::DRUSH_BIN)
+      ->dir(self::DRUPAL_ROOT);
+  }
 }
