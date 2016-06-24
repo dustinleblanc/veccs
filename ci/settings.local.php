a118efc64eb4fa27716e9aaa568bc47b9ba317c4
Dustin LeBlanc <dustin.m.leblanc@gmail.com>
2016-06-24 09:16:04 -0400

    update ci setup

---
+++ ci/settings.local.php
@@ -0,0 +1,12 @@
+<?php
+$settings['hash_salt'] = 'mmm...salty';
+$databases['default']['default'] = [
+  'database' => 'circle_test',
+  'username' => 'ubuntu',
+  'password' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'driver' => 'mysql',
+  'prefix' => '',
+  'collation' => 'utf8mb4_general_ci',
+];
