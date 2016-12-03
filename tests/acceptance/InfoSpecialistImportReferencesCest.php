310a3a1b83c759484b9475bfe688394a79a1c2cb
Dustin LeBlanc <dustin.m.leblanc@gmail.com>
2016-12-02 22:03:54 -0500

    Update test config and acceptance tests

--- tests/acceptance/InfoSpecialistImportReferencesCest.php
+++ tests/acceptance/InfoSpecialistImportReferencesCest.php
@@ -3,12 +3,17 @@
 use VCR\VCR;
 
 class InfoSpecialistImportReferencesCest {
+  private $itemID;
+
   public function _before(AcceptanceTester $I) {
+    $this->itemID = '5BPT7QEN';
     $I->loginAs('information_specialist');
+
     $I->haveFields([
       'field_zotero_user_id' => getenv('TEST_ZOTERO_USER_ID'),
       'field_zotero_api_key' => getenv('TEST_ZOTERO_API_KEY')
     ]);
+    $this->ensureItemNotInDb($this->itemID);
   }
 
   public function _after(AcceptanceTester $I) {
@@ -24,11 +29,21 @@ class InfoSpecialistImportReferencesCest {
     $I->amOnPage('/dashboard');
     $I->click('#edit-fetch-all');
     $I->waitForElementVisible('#zotero-collections', 30);
-    $I->click('//*[@id="zotero-item-5BPT7QEN"]/a');
-    $I->waitForElementVisible('//*[@id="success-zotero-5BPT7QEN"]');
+    $I->click("//*[@id=\"zotero-item-{$this->itemID}\"]/a");
+    $I->waitForElementVisible("//*[@id=\"success-zotero-{$this->itemID}\"]");
     $I->see('Item successfully imported!');
 
     VCR::eject();
     VCR::turnOff();
+  }
+
+  protected function ensureItemNotInDb($id) {
+    $result = \Drupal::entityQuery('research_reference_entity')
+      ->condition('zoteroKey', $id, '=')
+      ->execute();
+    if (!empty($result)) {
+      $item = \Drupal\zotero_import\Entity\ResearchReferenceEntity::load(array_pop($result));
+      $item->delete();
+    }
   }
 }
