b17e0e970a54ee12eb799938414c62b63b733040
Dustin LeBlanc <dustin.m.leblanc@gmail.com>
2017-02-08 22:49:19 -0500

    Add scripts from example-drops-8-composer

--- web/private/scripts/drush_config_import.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Import all config changes.
-echo "Importing configuration from yml files...\n";
-passthru('drush config-import -y');
-echo "Import of configuration complete.\n";
-//Clear all cache
-echo "Clearing cache.\n";
-passthru('drush cc all');
-echo "Clearing cache complete.\n";
